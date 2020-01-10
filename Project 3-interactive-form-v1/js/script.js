bed74450df67e6c9bfdb2381b4afd7d8c6d4ffe7
Tammi Carter <55289618+tammicarter@users.noreply.github.com>
2020-01-10 09:45:09 -0500

    Add files via upload

---
+++ Project 3-interactive-form-v1/js/script.js
@@ -0,0 +1,6 @@
+//*checking to assure that this script is set up properly//
+ alert("Im connected");
+//This is to prevent any jQuery code from running before the document is finished loading
+// $(document).ready(function(){
+
+// });
