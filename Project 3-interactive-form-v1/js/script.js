204e022a88730d26faec8fe91be8876b8295c66f
Tammi Carter <55289618+tammicarter@users.noreply.github.com>
2020-01-15 14:43:41 -0500

    Added Normalize CSS- and CDN for JQuery

--- Project 3-interactive-form-v1/js/script.js
+++ Project 3-interactive-form-v1/js/script.js
@@ -1,6 +1,7 @@
 //*checking to assure that this script is set up properly//
- alert("Im connected");
-//This is to prevent any jQuery code from running before the document is finished loading
-// $(document).ready(function(){
+ //alert("Im connected");
+//This is to prevent any jQuery code from running before the document is finished loading also to focus my 1st text field
+$(document).ready(function(){
+    $('form:first *:input[type!=hidden]:first').focus();
+});
 
-// });
